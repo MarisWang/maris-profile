d4656a2f1bd7f2cd2338b3613905d214dcf67452
maris <mariswang33@gmail.com>
2020-02-12 16:43:02 +0800

    new

---
+++ src/components/headPage.vue
@@ -0,0 +1,75 @@
+<script>
+import HeaderMenu from './headerMenu'
+import PersonalInformation from './personalInformation'
+import TalenShow from './talenShow'
+import Experience from './projects'
+import Contact from './contact'
+import WorkExperience from './workExperience'
+
+export default {
+  name: 'headPage',
+  components: {
+    HeaderMenu,
+    PersonalInformation,
+    TalenShow,
+    Experience,
+    Contact,
+    WorkExperience
+  }
+}
+</script>
+<template>
+  <main class="pageContent">
+    <div class="fixedBgImgs bg1">
+      <HeaderMenu/>
+    </div>
+    <div class="fixedBgImgs bg2">
+      <PersonalInformation/>
+    </div>
+    <div class="fixedBgImgs bg3">
+      <TalenShow/>
+    </div>
+    <div class="fixedBgImgs bg5">
+      <Experience/>
+    </div>
+    <div class="fixedBgImgs bg4">
+      <WorkExperience/>
+    </div>
+    <div class="fixedBgImgs bg6">
+      <Contact/>
+    </div>
+  </main>
+</template>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .fixedBgImgs {
+    min-height: 100vh;
+    background-size: cover;
+    background: no-repeat fixed center center;
+    display: flex;
+    align-items: center;
+  }
+
+  .fixedBgImgs.bg1 {
+    background-image: url("../assets/bg-01.jpg");
+  }
+
+  .fixedBgImgs.bg2 {
+    background-image: url("../assets/bg-02.jpg");
+  }
+
+  .fixedBgImgs.bg3 {
+    background-image: url("../assets/bg-03.jpg");
+  }
+
+  .fixedBgImgs.bg4 {
+    background-image: url("../assets/bg-08.jpg");
+  }
+  .fixedBgImgs.bg5 {
+    background-image: url("../assets/bg-05.jpg");
+  }
+  .fixedBgImgs.bg6 {
+    background-image: url("../assets/bg-06.jpg");
+  }
+</style>
---
+++ src/components/workExperience.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="workExperience">
+    <h2>工作经历</h2>
+    <ul>
+      <li v-for="(item,index) in experiences" :key="index">
+        <h5>时间：{{item.time}}</h5>
+        <h5>公司：{{item.company}}</h5>
+        <h5>部门：{{item.apartment}}</h5>
+        <h5>职位：{{item.position}}</h5>
+        <h5>离职原因：{{item.resaon}}</h5>
+      </li>
+    </ul>
+    <br/>
+    <p>熟练掌握ReactJS、VUEJS、以及AngularJS，在nodeJS的开发环境下，快速搭建项目工程，以UI设计为参考，
+      通过精简的HTML和CSS代码，再结合Ant-design 或者Element-UI等入门轻便的类库，99%UI设计页面还原；
+      然后，axios或者Fetch、apollo-fetch获取APi数据，通过都返回数据的解析，将数据和页面内容联系起来；
+      接下来、借以wow.js为页面添加些许动画效果，使得页面更加的生动；最后，配合测试人员，以有效的和
+      快速的方式，解决页面存在的问题，使得用户有更加流畅、快捷的体验。</p>
+    <a href="https://resume-f86fc.web.app/#/" target="_blank">https://resume-f86fc.web.app/#/</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'workExperience',
+  data () {
+    return {
+      experiences: [
+        {
+          time: '2018.01-目前',
+          company: '科技狗',
+          apartment: '信息系统二次开发',
+          position: 'web前端开发工程师',
+          resaon: '在职'
+        },
+        {
+          time: '2017.07-2018.01',
+          company: '中软国际',
+          apartment: '汇丰银行第四事业开发部',
+          position: 'web前端开发工程师',
+          resaon: '出国谋求更好发展和生活'
+        },
+        {
+          time: '2015.03-2017.06',
+          company: '软通动力',
+          apartment: '华为业务线  SPO实施部',
+          position: 'web前端开发工程师',
+          resaon: '技能提升'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.workExperience{
+  width: 90%;
+  padding: 20px 45px;
+  margin: 0 auto;
+  background: rgba(255, 255, 255, 0.4);
+}
+.workExperience ul {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.workExperience ul li{
+  width: 300px;
+  line-height: 30px;
+  margin: 10px auto;
+  box-shadow: 0 10px 10px 0 rgba(0,0,0,0.50);
+}
+.workExperience p{
+  text-align: left;
+  text-indent: 30px;
+  line-height: 25px;
+}
+.workExperience a{
+  text-align: left;
+  display: block;
+  color: #008fd1;
+}
+.workExperience a:hover{
+  text-decoration: underline;
+}
+</style>
